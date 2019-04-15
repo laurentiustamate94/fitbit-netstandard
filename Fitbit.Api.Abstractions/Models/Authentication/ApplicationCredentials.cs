30074daf9c71094432801f484f1b5ac84b3541e4
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2019-04-15 22:37:41 +0200

    Add configuration class to handle client secrets

---
+++ Fitbit.Api.Abstractions/Models/Authentication/ApplicationCredentials.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fitbit.Api.Abstractions.Models.Authentication
+{
+    public class ApplicationCredentials
+    {
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public Uri RedirectUri { get; set; }
+
+        public bool UseOwnCsrfProtection { get; set; }
+
+        public ApplicationCredentials()
+        {
+        }
+
+        public ApplicationCredentials(string clientId, string clientSecret, string redirectUri, bool useOwnCsrfProtection)
+            : this(clientId, clientSecret, new Uri(redirectUri), useOwnCsrfProtection)
+        {
+        }
+
+        public ApplicationCredentials(string clientId, string clientSecret, Uri redirectUri, bool useOwnCsrfProtection)
+        {
+            this.ClientId = clientId;
+            this.ClientSecret = clientSecret;
+            this.RedirectUri = redirectUri;
+            this.UseOwnCsrfProtection = useOwnCsrfProtection;
+        }
+
+        public bool IsValid()
+        {
+            if (string.IsNullOrWhiteSpace(ClientId))
+            {
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(ClientSecret))
+            {
+                return false;
+            }
+
+            //TODO check for RedirectUri
+
+            return true;
+        }
+    }
+}
