de4e5634595c3f71a369fd513776a0c32b1696d3
Laurentiu Stamate <laurentiu.stamate@outlook.com>
2019-04-15 22:41:20 +0200

    Add fitbit client interface and implementation

---
+++ Fitbit.Api.Abstractions/IFitbitClient.cs
@@ -0,0 +1,28 @@
+﻿using Fitbit.Api.Abstractions.Endpoints;
+using System;
+
+namespace Fitbit.Api.Abstractions
+{
+    public interface IFitbitClient
+    {
+        IAuthentication Authentication { get; }
+
+        //IActivity Activity { get; }
+
+        //IBodyAndWeight BodyAndWeight { get; }
+
+        //IDevices Devices { get; }
+
+        //IFoodLogging FoodLogging { get; }
+
+        //IFriends Friends { get; }
+
+        IHeartRate HeartRate { get; }
+
+        //ISleep Sleep { get; }
+
+        //ISubscriptions Subscriptions { get; }
+
+        //IUser User { get; }
+    }
+}
---
+++ Fitbit.Api/FitbitClient.cs
@@ -0,0 +1,32 @@
+﻿using Fitbit.Api.Abstractions;
+using Fitbit.Api.Abstractions.Endpoints;
+using Fitbit.Api.Abstractions.Models.Authentication;
+using Fitbit.Api.Endpoints;
+using System;
+
+namespace Fitbit.Api
+{
+    public class FitbitClient : IFitbitClient
+    {
+        public IAuthentication Authentication { get; }
+
+        public IHeartRate HeartRate { get; }
+
+        public FitbitClient(string clientId, string clientSecret, string redirectUri, bool useOwnCsrfProtection = true)
+            : this(new ApplicationCredentials(clientId, clientSecret, redirectUri, useOwnCsrfProtection))
+        {
+        }
+
+        public FitbitClient(ApplicationCredentials applicationCredentials)
+        {
+            if (!applicationCredentials.IsValid())
+            {
+                //TODO throw Exception
+            }
+
+            this.Authentication = new Authentication(applicationCredentials);
+
+            this.HeartRate = new HeartRate(Authentication);
+        }
+    }
+}
